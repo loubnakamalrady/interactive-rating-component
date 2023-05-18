c610302d682cdc4b50229aad576e7f9635bbaa85
Loubna Kamal <loubnakamaly1998@gmail.com>
2023-05-18 16:31:26 +0300

    fourth

--- dynamic.js
+++ dynamic.js
@@ -2,9 +2,21 @@ const mainContainer = document.querySelector(".main-container");
 const thanksContainer = document.querySelector(".thanks-container");
 const selectOut = document.querySelector(".selection");
 const submitBtn = document.querySelector(".submit-button");
+const selectRate = document.querySelector("#select");
+const selectAll = document.querySelectorAll(".btnslct");
 
 submitBtn.addEventListener("click", () =>{
     mainContainer.style.display = "none";
     thanksContainer.classList.remove("hidden");
 });
+
+selectAll.forEach((theSelect) => {
+    theSelect.addEventListener("click", () => {
+        selectRate.innerHTML = theSelect.innerHTML;
+    });
+});
+
+selectRate
+
+
 
